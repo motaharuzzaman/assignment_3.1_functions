18661b03becdf6ca3f8262e3e0e597461e2145eb
Md. Motaharuzzaman <motaharuzzaman71@gmail.com>
2023-10-16 18:23:51 +0600

    Q-1 List average completed

--- functions.py
+++ functions.py
@@ -1,3 +1,12 @@
 """
 1. Create a function to calculate the average number of a given list.
 """
+def average(my_list):
+    if len(my_list)==0:
+        return 0
+    total = sum(my_list)
+    list_average = total/len(my_list)
+    return list_average
+my_list = [4,6,9,8,8]
+result = average(my_list)
+print(f"The average of the numbers containing in the list is : {result}")
